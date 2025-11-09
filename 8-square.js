ca973131c970c8ee4ef03393f1adfb1c3b094500
Seba Ndandala <sebandandala99@gmail.com>
2025-11-09 22:12:06 +0300

    alx

--- 8-square.js
+++ 8-square.js
@@ -2,7 +2,7 @@ const { argv } = require('node:process');
 
 if (!isNaN(Number(argv[2]))){
     let j = Number(argv[2])
-    let row = "*".repeat(j)
+    let row = "X".repeat(j)
     for (let i = 0; i < j; i++){
         
         console.log(row);
